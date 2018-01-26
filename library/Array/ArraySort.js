b354b70958a9363b328f50496a35dc3c1fd104c0
linweinb <1021069119@qq.com>
2018-01-26 11:42:23 +0800

    新增选择排序方法
    Signed-off-by: linweinb <1021069119@qq.com>

--- library/Array/ArraySort.js
+++ library/Array/ArraySort.js
@@ -6,21 +6,49 @@
  */
 
 
+ // The object is about sharing function.
 
- // Bubble sort.The result is from small to large.
+ var sharingFunction={
+
+ 	// Exchange two numbers.
+ 	exchangeNumber:function(index1,index2){
+ 		var temp;
+ 		temp=this[index1];
+ 		this[index1]=this[index2];
+ 		this[index2]=temp;
+ 	}
+
+ };
+
+
+
+ // Bubble Sort.The result is from small to large.
 
 Array.prototype.bubbleSort=function(){
-	var temp;
-	function exchangeNumber(a,b){
-		temp=this[a];
-		this[a]=this[b];
-		this[b]=temp;
-	}
 	for(var i=0,len1=this.length;i<len1;i+=1){
 		for(var j=0,len2=this.length-1-i;j<len2;j+=1){
 			if(this[j]>this[j+1]){
-				exchangeNumber.apply(this,[j,j+1]);
+				sharingFunction.exchangeNumber.apply(this,[j,j+1]);
 			}
 		}
 	}
+};
+
+
+
+// Selection Sort.The result is from small to large.
+
+Array.prototype.selectionSort=function(){
+    var indexMin;                                     // The index of current minimum.
+    for(var i=0,len1=this.length-1;i<len1;i+=1){
+    	indexMin=i;
+    	for(var j=i,len2=this.length;j<len2;j+=1){
+            if(this[indexMin]>this[j]){
+            	indexMin=j;
+            }
+    	}
+    	if(i!==indexMin){
+    		sharingFunction.exchangeNumber.apply(this,[i,indexMin]);
+    	}
+    }
 };
