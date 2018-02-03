67643ce634c10a80c031c5d5e5064e29aa6654c2
linweinb <1021069119@qq.com>
2018-02-03 22:03:32 +0800

    重写

--- library/Array/ArraySort.js
+++ library/Array/ArraySort.js
@@ -6,13 +6,31 @@
  */
 
 
+// The object is about sharing function.
+
+ var sharingFunction={};
+
+
 // All the methods cannot be modified.
 
 // The method of sharingFunction.
 Object.defineProperties(sharingFunction,{
 
+    // exchange two number.
     exchangeNumber:{
-        configurable:false
+        value:function(index1,index2){
+            var temp;
+            temp=this[index1];
+            this[index1]=this[index2];
+            this[index2]=temp;
+        },
+
+        // Cannot be modified. 
+        writable:false,
+
+        configurable:false,
+
+        enumerable:true
     }
     
 });
@@ -20,89 +38,77 @@ Object.defineProperties(sharingFunction,{
 // The method of Array.prototype.
 Object.defineProperties(Array.prototype,{
 
+    // Bubble Sort.The result is from small to large.
     bubbleSort:{
-        configurable:false
+        value:function(){
+            for(var i=0,len1=this.length;i<len1;i+=1){
+                for(var j=0,len2=this.length-1-i;j<len2;j+=1){
+                    if(this[j]>this[j+1]){
+                        sharingFunction.exchangeNumber.apply(this,[j,j+1]);
+                    }
+                }
+            }
+        },
+
+        // Cannot be modified.
+        writable:false,
+
+        configurable:false,
+
+        enumerable:true
     },
 
+    // Selection Sort.The result is from small to large.
     selectionSort:{
-        configurable:false
+        value:function(){
+            var indexMin;                                     // The index of current minimum.
+            for(var i=0,len1=this.length-1;i<len1;i+=1){
+                indexMin=i;
+                for(var j=i,len2=this.length;j<len2;j+=1){
+                    if(this[indexMin]>this[j]){
+                        indexMin=j;
+                    }
+                }
+                if(i!==indexMin){
+                    sharingFunction.exchangeNumber.apply(this,[i,indexMin]);
+                }
+            }
+        },
+
+        // Cannot be modified.
+        writable:false,
+
+        configurable:false,
+
+        enumerable:true
     },
 
+    // Insertion Sort.The result is from small to large.
     insertionSort:{
-        configurable:false
+        value:function(){
+            var temp,
+                i,
+                j;
+            for(i=1;i<this.length;i+=1){
+                j=i;
+                temp=this[i];               // The variable temp as the second element of the array is the initial value of the element to be compared.
+
+                // If j>0 and the previous element is bigger than the variable temp , continue the loop. 
+
+                while(j>0&&this[j-1]>temp){
+                    this[j]=this[j-1];
+                    j--;
+                }
+                this[j]=temp;               // Finally,the temp is the first element of the array.
+            }    
+        },
+
+        // Cannot be modified.
+        writable:false,
+
+        configurable:false,
+
+        enumerable:true
     }
 
 });
-
-
-
-
-
- // The object is about sharing function.
-
- var sharingFunction={
-
- 	// Exchange two numbers.
- 	exchangeNumber:function(index1,index2){
- 		var temp;
- 		temp=this[index1];
- 		this[index1]=this[index2];
- 		this[index2]=temp;
- 	}
-
- };
-
-
-
- // Bubble Sort.The result is from small to large.
-
-Array.prototype.bubbleSort=function(){
-	for(var i=0,len1=this.length;i<len1;i+=1){
-		for(var j=0,len2=this.length-1-i;j<len2;j+=1){
-			if(this[j]>this[j+1]){
-				sharingFunction.exchangeNumber.apply(this,[j,j+1]);
-			}
-		}
-	}
-};
-
-
-
-// Selection Sort.The result is from small to large.
-
-Array.prototype.selectionSort=function(){
-    var indexMin;                                     // The index of current minimum.
-    for(var i=0,len1=this.length-1;i<len1;i+=1){
-    	indexMin=i;
-    	for(var j=i,len2=this.length;j<len2;j+=1){
-            if(this[indexMin]>this[j]){
-            	indexMin=j;
-            }
-    	}
-    	if(i!==indexMin){
-    		sharingFunction.exchangeNumber.apply(this,[i,indexMin]);
-    	}
-    }
-};
-
-
-
-// Insertion Sort.The result is from small to large.
-
-Array.prototype.insertionSort=function(){
-    var temp,
-        i,
-        j;
-    for(i=1;i<this.length;i+=1){
-        j=i;
-        temp=this[i];               // The variable temp as the second element of the array is the initial value of the element to be compared.
-
-        // If j>0 and the previous element is bigger than the variable temp , continue the loop. 
-
-        while(j>0&&this[j-1]>temp){
-            this[j]=this[j-1];
-            j--;
-        }
-        this[j]=temp;               // Finally,the temp is the first element of the array.
-    }    
-};
