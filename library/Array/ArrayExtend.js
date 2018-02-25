11d418281bfb6f17b10245c55758dac53e1544b5
linweinb <1021069119@qq.com>
2018-02-25 10:02:39 +0800

    Signed-off-by: linweinb <1021069119@qq.com>

--- library/Array/ArrayExtend.js
+++ library/Array/ArrayExtend.js
@@ -4,109 +4,114 @@
  */
 
 
-// 共享接口
-function ShareExtend(){
+;(function(){
+    // 共享接口
+    function Share(){
 
-	// 定义将一个数组的元素推入另一个数组的方法，参数array2的所有元素都推入array1
-	this.pushArray=function(array1,array2){
-    	for(var i=0,len=array2.length;i<len;i+=1){
-    		array1.push(array2[i]);
-    	}
-	};
+        // 定义将一个数组的元素推入另一个数组的方法，参数array2的所有元素都推入array1
+        this.pushArray=function(array1,array2){
+            for(var i=0,len=array2.length;i<len;i+=1){
+                array1.push(array2[i]);
+            }
+        };
 
-} 
+    } 
 
-var shareExtend=new ShareExtend();
+    var share=new Share();
 
-// 冻结共享实例
-Object.freeze(shareExtend);
+    // 冻结共享实例
+    Object.freeze(share);
 
-// 扩展方法
-Object.defineProperties(Array.prototype,{
+    // 扩展方法
+    Object.defineProperties(Array.prototype,{
 
-	// 与原生concat类似的方法
-	__concat:{
-		value:function(){
-			var array=[],      // 将要作为返回值的函数
-			    i,
-			    j,
-			    len1,
-			    len2;
-			if(!arguments.length){            // 如果没有参数
-				for(i=0,len1=this.length;i<len1;i+=1){
-					array.push(this[i]);               // 将本数组的元素推入array
-				}
-                return array;
-			} else{  // 如果有参数
-            	for(i=0,len1=this.length;i<len1;i+=1){  // 将本数组的元素推入array
-            		array.push(this[i]);
-            	}
-            	for(i=0,len1=arguments.length;i<len1;i+=1){   // 接着遍历参数
-            		// 参数是数组
-                	if(Object.prototype.toString.apply(arguments[i])==="[object Array]"){
-                    	for(j=0,len2=arguments[i].length;j<len2;j+=1){    // 遍历数组
-                        	array.push(arguments[i][j]);                // 将参数数组的元素推入array
-                    	}
-                	} else{
-                		array.push(arguments[i]);    // 将其他类型参数推入array
-                	}
-            	}
-            	return array;
-			}
-		},
+        // 与原生concat类似的方法
+        __concat:{
+            value:function(){
+                var array=[],      // 将要作为返回值的函数
+                    i,
+                    j,
+                    len1,
+                    len2;
+                if(!arguments.length){            // 如果没有参数
+                    for(i=0,len1=this.length;i<len1;i+=1){
+                        array.push(this[i]);               // 将本数组的元素推入array
+                    }
+                    return array;
+                } else{  // 如果有参数
+                    for(i=0,len1=this.length;i<len1;i+=1){  // 将本数组的元素推入array
+                        array.push(this[i]);
+                    }
+                    for(i=0,len1=arguments.length;i<len1;i+=1){   // 接着遍历参数
+                        // 参数是数组
+                        if(Object.prototype.toString.apply(arguments[i])==="[object Array]"){
+                            for(j=0,len2=arguments[i].length;j<len2;j+=1){    // 遍历数组
+                                array.push(arguments[i][j]);                // 将参数数组的元素推入array
+                            }
+                        } else{
+                            array.push(arguments[i]);    // 将其他类型参数推入array
+                        }
+                    }
+                    return array;
+                }
+            },
 
-		// 无法修改
-		writable:false,
+            // 无法修改
+            writable:false,
 
-		// 无法重新配置
-		configurable:false,
+            // 无法重新配置
+            configurable:false,
 
-		// 可以被枚举
-		enumerable:true
-	},
+            // 可以被枚举
+            enumerable:true
+        },
 
-	// 将数组分割成指定长度的子数组，这个方法创建并返回一个新数组，这些子数组将作为新数组的元素
-    __chunk:{
-    	value:function(size){     // size为子数组的长度
-    		var storeArray=[],    // 将要返回的新数组
-    		    chunk=[],         // 子数组
-                i;
-            switch(true){
-            	// 如果本数组长度为0
-            	case !this.length:
-                     return storeArray;  // 新数组将作为空数组返回
-                // 如果本数组长度大于0且size为0或不指定子数组长度
-                case this.length>0&&(size===0||size===undefined):
-                     shareExtend.pushArray(storeArray,this);
-                     return storeArray;
-                // 如果本数组长度大于0且size大于0
-                case this.length>0&&size>0:
-                     if(size>=this.length){      // 如果指定的长度大于或等于本数组长度则返回本数组的副本
-                     	shareExtend.pushArray(storeArray,this);
-                     	return storeArray;
-                     } else{                    // 否则正常分组
-                     	for(i=0,len=this.length;i<len;i+=1){   // 遍历本数组
-                     		chunk.push(this[i]);          
-                     		if((i+1)%size===0){        // 如果遍历到本数组指定的长度，则将子数组推入新数组
-                     			storeArray.push(chunk);
-                     			chunk=[];       
-                     		}
-                     	}
-                     	if(chunk.length){      // 将本数组剩余元素作为子数组的元素推入新数组
-                     		storeArray.push(chunk);
-                     	}
-                     	return storeArray;
-                     }          
-            }    
-    	},
+        // 将数组分割成指定长度的子数组，这个方法创建并返回一个新数组，这些子数组将作为新数组的元素
+        __chunk:{
+            value:function(size){     // size为子数组的长度
+                var storeArray=[],    // 将要返回的新数组
+                    chunk=[],         // 子数组
+                    i;
+                switch(true){
+                    // 如果本数组长度为0
+                    case !this.length:
+                        return storeArray;  // 新数组将作为空数组返回
+                    // 如果本数组长度大于0且size为0或不指定子数组长度
+                    case this.length>0&&(size===0||size===undefined):
+                        share.pushArray(storeArray,this);
+                        return storeArray;
+                    // 如果本数组长度大于0且size大于0
+                    case this.length>0&&size>0:
+                        if(size>=this.length){      // 如果指定的长度大于或等于本数组长度则返回本数组的副本
+                            share.pushArray(storeArray,this);
+                            return storeArray;
+                        } else{                    // 否则正常分组
+                            for(i=0,len=this.length;i<len;i+=1){   // 遍历本数组
+                                chunk.push(this[i]);          
+                                if((i+1)%size===0){        // 如果遍历到本数组指定的长度，则将子数组推入新数组
+                                    storeArray.push(chunk);
+                                    chunk=[];       
+                                }
+                            }
+                            if(chunk.length){      // 将本数组剩余元素作为子数组的元素推入新数组
+                                storeArray.push(chunk);
+                            }
+                            return storeArray;
+                        }          
+                    // 如果size不是数字
+                    case typeof size!=="number"&&size!==undefined:
+                         throw new Error("patameter error:size is not a number.");    
+                }    
+            },
 
-    	// 不可修改
-    	writable:false,
+            // 不可修改
+            writable:false,
 
-    	// 不可重新配置
-    	configurable:false,
+            // 不可重新配置
+            configurable:false,
 
-    	// 可以被枚举
-    	enumerable:true
-    }
-});
+            // 可以被枚举
+            enumerable:true
+        }
+    });
+})();
--- library/Array/ArraySort.js
+++ library/Array/ArraySort.js
@@ -6,132 +6,105 @@
  */
 
 
-// 共享构造函数
+;(function(){
 
-function ShareFunctionSort(){
-    Object.defineProperties(this,{
+    // 共享接口
+    function Share(){
 
-        // 两数交换方法
-        exchangeNumber:{
-
-            value:function(index1,index2){
-                var temp;
-                temp=this[index1];
-                this[index1]=this[index2];
-                this[index2]=temp;
-            },
-
-            // 无法修改
-            writable:false,
-
-            // 无法配置
-            configurable:false,
-
-            // 可以枚举出来
-            enumerable:true
-            
-        }
-    });
-}
-
-
-// 共享函数实例
-
-var sharingFunctionSort=new ShareFunctionSort();
-
-
-
-// Array.prototype上的方法。
-
-Object.defineProperties(Array.prototype,{
-
-    // 冒泡排序。数组元素从小到大排序。
-
-    bubbleSort:{
-        value:function(){
-            for(var i=0,len1=this.length;i<len1;i+=1){
-                for(var j=0,len2=this.length-1-i;j<len2;j+=1){
-                    if(this[j]>this[j+1]){
-                        sharingFunctionSort.exchangeNumber.apply(this,[j,j+1]);
-                    }
-                }
-            }
-        },
-
-        // 无法更改。 
-
-        writable:false,
-
-        // 无法配置。
-
-        configurable:false,
-
-        // 可以枚举。
-
-        enumerable:true
-    },
-
-    // 选择排序。数组元素从小到大排序。
-
-    selectionSort:{
-        value:function(){
-            var indexMin;                                     // 当前最小元素的索引。
-            for(var i=0,len1=this.length-1;i<len1;i+=1){
-                indexMin=i;
-                for(var j=i,len2=this.length;j<len2;j+=1){
-                    if(this[indexMin]>this[j]){
-                        indexMin=j;
-                    }
-                }
-                if(i!==indexMin){
-                    sharingFunctionSort.exchangeNumber.apply(this,[i,indexMin]);
-                }
-            }
-        },
-
-        // 无法更改。
-
-        writable:false,
-
-        // 无法配置。
-
-        configurable:false,
-
-        // 可以枚举。
-
-        enumerable:true
-    },
-
-    // 插入排序。数组元素从小到大排序。
-
-    insertionSort:{
-        value:function(){
-            var temp,
-                i,
-                j;
-            for(i=1;i<this.length;i+=1){
-                j=i;
-                temp=this[i];              
-                while(j>0&&this[j-1]>temp){
-                    this[j]=this[j-1];
-                    j--;
-                }
-                this[j]=temp;              
-            }    
-        },
-
-        // 无法更改。
-
-        writable:false,
-
-        // 无法配置。
-
-        configurable:false,
-
-       // 可以枚举。
-       
-        enumerable:true
+       // 两数交换方法
+       this.exchangeNumber=function(index1,index2){
+            var temp;
+            temp=this[index1];
+            this[index1]=this[index2];
+            this[index2]=temp;
+       };
 
     }
 
-});
+    // 共享函数实例
+    var share=new Share();
+    
+    // 冻结实例
+    Object.freeze(share);
+
+    // Array.prototype上的方法。
+    Object.defineProperties(Array.prototype,{
+
+        // 冒泡排序。数组元素从小到大排序。
+        bubbleSort:{
+            value:function(){
+                for(var i=0,len1=this.length;i<len1;i+=1){
+                    for(var j=0,len2=this.length-1-i;j<len2;j+=1){
+                        if(this[j]>this[j+1]){
+                            share.exchangeNumber.apply(this,[j,j+1]);
+                        }
+                    }
+                }
+            },
+
+            // 无法更改。 
+            writable:false,
+
+            // 无法配置。
+            configurable:false,
+
+            // 可以枚举。
+            enumerable:true
+        },
+
+        // 选择排序。数组元素从小到大排序。
+        selectionSort:{
+            value:function(){
+                var indexMin;                                     // 当前最小元素的索引。
+                for(var i=0,len1=this.length-1;i<len1;i+=1){
+                    indexMin=i;
+                    for(var j=i,len2=this.length;j<len2;j+=1){
+                        if(this[indexMin]>this[j]){
+                            indexMin=j;
+                        }
+                    }
+                    if(i!==indexMin){
+                        share.exchangeNumber.apply(this,[i,indexMin]);
+                    }
+                }
+            },
+
+            // 无法更改。
+            writable:false,
+
+            // 无法配置。
+            configurable:false,
+
+            // 可以枚举。
+            enumerable:true
+        },
+
+        // 插入排序。数组元素从小到大排序。
+        insertionSort:{
+            value:function(){
+                var temp,
+                    i,
+                    j;
+                for(i=1;i<this.length;i+=1){
+                    j=i;
+                    temp=this[i];              
+                    while(j>0&&this[j-1]>temp){
+                        this[j]=this[j-1];
+                        j--;
+                    }
+                    this[j]=temp;              
+                }    
+            },
+
+            // 无法更改。
+            writable:false,
+
+            // 无法配置。
+            configurable:false,
+
+            // 可以枚举。
+            enumerable:true
+
+        }
+    });
+})();
