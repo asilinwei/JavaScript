ef4c5a54dba5ca9bfba67d388689793ba424c949
linweinb <1021069119@qq.com>
2018-05-19 18:21:52 +0800

    Signed-off-by: linweinb <1021069119@qq.com>

---
+++ toy/compact.js
@@ -0,0 +1,37 @@
+/*
+ * 2018-05-19
+   @linweinb
+
+   Remove all the falsy element, return a new array.
+
+   For example:
+    var array=[1,0,null,7,''];
+
+    console.log(compact(array));  // => [1,7]
+*/    
+
+
+
+if(!window.compact){
+	var compact=(function(){
+		"use strict";
+		var isArray=function(array){
+			var toString=Object.prototype.toString;
+			return toString.apply(array)==='[object Array]';
+		};
+
+		return function(array){
+			if(isArray(array)&&array.length){
+				var result=[];
+				for(var i=0;i<array.length;i+=1){
+					if(array[i]){
+						result.push(array[i]);
+					}
+				}
+			} else{
+				return [];
+			}
+			return result;
+		};
+	})();
+}
