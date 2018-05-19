a0f6c0f78ecbf36dca032f2d9b7a0700bfa9a15c
linweinb <1021069119@qq.com>
2018-05-19 16:37:03 +0800

    Signed-off-by: linweinb <1021069119@qq.com>

---
+++ toy/chunk.js
@@ -0,0 +1,69 @@
+/*
+ * 2018-05-19
+   @linweinb
+   
+   Split array into groups the length of size, return a new array.
+
+   Syntax:
+   	chunk(array,[size=array.length]);	
+
+   Arguments
+   	array(Array): The array to split into.
+	[size=array.length](number): The length of each chunk.
+   
+   For example:
+   var array=[1,2,3,4,5,6];
+
+   console.log(chunk(array));  // => [1,2,3,4,5,6]
+
+   console.log(chunk(array,2));  // => [[1,2],[3,4],[5,6]]
+
+   console.log(chunk(array,3));  // => [[1,2,3],[4,5,6]]	
+
+   console.log(chunk(array,4));  // => [[1,2,3,4],[5,6]]	
+
+*/
+
+if(!window.chunk){
+	var chunk=(function(){
+		"use strict";
+
+		// check if it is an array.
+		var isArray=function(array){
+			var toString=Object.prototype.toString;
+			return toString.apply(array)==='[object Array]';
+		};
+
+		// check if it is a number.
+		var isNumber=function(value){
+			return typeof value==='number'&&isFinite(value);
+		};
+
+		// push element into array.
+		var push=function(array1,array2,array3,n){
+			array3.push(array1[n]);
+			if(n===array1.length-1){
+				array2.push(array3);
+			}
+		};
+		return function(array,size){
+			if(isArray(array)&&array.length){
+				size=size||array.length;   // the defalut length of each chunk.
+				if(isNumber(size)){
+					var result=[];   // the new array to return.
+					var section=[];  // each chunk.
+					for(var i=0;i<array.length;i+=1){
+						if(section.length<size){
+							push(array,result,section,i);
+							continue;
+						}
+						result.push(section);
+						section=[];
+						push(array,result,section,i);
+					}
+				}
+			}
+			return result;
+		};
+	})();
+}
