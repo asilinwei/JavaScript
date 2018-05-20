3e75697e742e76648c4a085d38a88ff449725ab3
linweinb <1021069119@qq.com>
2018-05-20 14:40:29 +0800

    Signed-off-by: linweinb <1021069119@qq.com>

--- toy/drop.js
+++ toy/drop.js
@@ -95,3 +95,98 @@ if(!window.drop){
 		};
 	})();
 }
+
+
+
+/**
+ * 2018-05-20
+ * @linweinb
+ *
+ * dropRight:
+ * Drop elements from end with the specified number, return a new splice array.
+ *
+ * Syntax:
+ * dropRight(array,[n=0]);
+ *
+ * Arguments:
+ * array(Array): The array to query.
+ * [n=0](number): The number of elements to drop.
+ *
+ *
+ *
+ *
+ * For example:
+ *
+ * var array=[1,2,3,4,5];
+ * 
+ * console.log(dropRight(array));  // => [1,2,3,4,5]
+ *
+ * console.log(dropRight(array,1));  // => [1,2,3,4]
+ *
+ * console.log(dropRight(array,2));  // => [1,2,3]
+ */
+
+
+if(!window.dropRight){
+	var dropRight=(function(){
+		"use strict";
+
+		// check if it is an array.
+		var isArray=function(array){
+			var toString=Object.prototype.toString;
+			return toString.apply(array)==='[object Array]';
+		};
+
+		// check if it is a number.
+		var isNumber=function(value){
+			return typeof value==='number'&&isFinite(value);
+		};
+
+		// check if it is a legal number.
+		var isOverflow=function(length,value){
+			return isNumber(length)&&
+				   isNumber(value)&&
+				   value>=0&&
+				   value<=length;
+		};
+
+		// check if it is a string.
+		var isString=function(value){
+			return typeof value==='string';
+		};
+
+		// throw the error.
+		var error=function(type,message){
+			if(isString(type)&&isString(message)){
+				throw {
+					type:type,
+					message:message
+				};
+			}
+		};
+		return function(array,n){
+			if(isArray(array)&&array.length){
+
+				var length=array.length;
+				n=n||0;  // the default number of elements to drop is zero.
+
+				if(isOverflow(length,n)){
+
+					var result=[];  // the new splice array to return.
+
+					for(var i=0;i<length;i+=1){
+						if(i===length-n){
+							break;
+						}
+						result.push(array[i]);
+					}
+				} else{
+					error('ArgumentsError','The number of elements to drop is error.');
+				}
+			} else{
+				return [];
+			}
+			return result;
+		};
+	})();
+}
