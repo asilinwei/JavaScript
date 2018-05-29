ca142340bb1ceb1fcee1ff3b696f5a6fa05c8680
linweinb <1021069119@qq.com>
2018-05-29 10:43:08 +0800

    Signed-off-by: linweinb <1021069119@qq.com>

---
+++ toy/trim.js
@@ -0,0 +1,49 @@
+/**
+ * 2018-05-29
+ * @linweinb
+ *
+ * trim:
+ * Get rid of the white space both sides of the string.
+ *
+ * Syntax:
+ * trim(string)
+ *
+ * Arguments:
+ * string(string): The string to query.
+ *
+ * Return:
+ * The trimmed string.
+ *
+ *
+ * For example:
+ *
+ * console.log(trim('    hello world    '));  // => 'hello world'
+ *
+ */
+
+
+if(!window.trim){
+	var trim=(function(){
+		"use strict";
+
+		// throw the error.
+		var CustomError=function(type,message){
+			this.type=type;
+			this.message=message;
+		};
+
+		// check if it is a string.
+		var isString=function(value){
+			return typeof value==='string';
+		};
+
+		return function(string){
+			if(isString(string)){
+				var match=string.match(/\b[a-z\s]+\b/ig);   // match the result.
+				return match.join('');
+			} else{
+				throw new CustomError('ArgumentsError','Not string.');
+			}
+		};
+	})();
+}
