89edccd481ba80c5dddfbcd4517a2e1fc7d8b981
asilinwei <1021069119@qq.com>
2018-11-07 15:15:22 +0800

    Signed-off-by: asilinwei <1021069119@qq.com>

---
+++ toy/merge.js
@@ -0,0 +1,69 @@
+if (!window.merge) {
+   var merge = (function() {
+      "use strict";
+
+      var size = function(obj) {
+         return obj.length;
+      };
+
+      var initialise = function(obj, key, value) {
+         obj[key] = [];
+         obj[key].flag = true;
+         obj[key].push(value);
+      };
+
+      var remove = function(obj) {
+         var keys = Object.keys(obj),
+             index = -1;
+
+         while (++index < size(keys)) {
+            if (Array.isArray(obj[keys[index]])) {
+               delete obj[keys[index]].flag;
+            }
+         }    
+      };
+
+      var check = function(result, obj, index, key) {
+         if (result.hasOwnProperty(key)) {
+            var resultValue = result[key];
+
+            if (Array.isArray(resultValue)) {
+               if (!resultValue.flag) {
+                  initialise(result, key, resultValue);
+               }
+            } else {
+               initialise(result, key, resultValue);
+            }
+            result[key].push(obj[index][key]);
+         } else {
+            result[key] = obj[index][key];
+         }
+      };
+
+      var travelKeys = function(result, obj, index) {
+         var keys = Object.keys(obj[index]),
+             keyIndex = -1;
+             
+         while (++keyIndex < size(keys)) {
+            check(result, obj, index, keys[keyIndex]);
+         }
+      };
+
+      var merge = function(obj) {
+         var result = {},
+             index = -1;
+
+         while (++index < size(obj)) {
+            if (typeof obj[index] === 'object' && obj[index] !== null) {
+               travelKeys(result, obj, index);
+            }
+         }
+         remove(result);
+         return result;
+      };
+
+      return function() {
+         return merge(arguments);
+      };
+   })();
+}
